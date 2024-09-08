94f782005451a4e8fa7ac3a1576ae8ed09ca6db7
Sharmayank18 <138447666+Sharmayank18@users.noreply.github.com>
2024-09-08 11:46:08 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,109 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "JJ sharma",
+        email: "jjsharma@gmail.com",
+      },
+      {
+        name: "Mayank sharma",
+        email: "sharmayank18@gmail.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "Papa ji",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Pushplata sharma",
+        email: "pushpa@gmai.com",
+      },
+      {
+        name: "Mayank sharma",
+        email: "sharmayank18@gmail.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Mummy",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Surbhi Sharma",
+        email: "Surbhi415@gmai.com",
+      },
+      {
+        name: "Mayank sharma",
+        email: "sharmayank18@gmail.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Subhu bro",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Anup sharma",
+        email: "anupsharma@gmail.com",
+      },
+      {
+        name: "Mayank sharma",
+        email: "sharmayank18@gmail.com",
+      },
+      {
+        name: "Krishna vashishta",
+        email: "kirrish@gmail.com",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "Brothers",
+    groupAdmin: {
+      name: "Mayank sharma",
+      email: "sharmayank18@gmail.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "krishna vashishta",
+        email: "krissh@gmail.com",
+      },
+      {
+        name: "Mayank sharma",
+        email: "sharmayank18@gmail.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Chotu bhaiya",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Tushar",
+        email: "tushar@gmail.com",
+      },
+      {
+        name: "Mayank",
+        email: "sharmayank18@gmail.com",
+      },
+      {
+        name: "Inderpal",
+        email: "inder@gmail.com",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Good Boyz",
+    groupAdmin: {
+      name: "Inderpal",
+      email: "inder@gmail.com",
+    },
+  },
+];
+export default chats  ;
